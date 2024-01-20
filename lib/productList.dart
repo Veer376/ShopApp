9e8cad5f7026ba8fb76c40b8bf7e5010f0fe7258
Veer376 <aryaveer.22gcebaiml050@galgotiacollege.edu>
2024-01-21 01:29:15 +0530

    dummy products

---
+++ lib/productList.dart
@@ -0,0 +1,48 @@
+
+List<Map<String,dynamic>> products=[
+  {
+  "id":"0",
+  "company": "Puma",
+  "title": "Men's Puma BMW Running Shoes",
+  'Size': [7,8,9,10],
+  'price': 44.5,
+    'Deal': " 29% OFF ",
+  'imageURL':'assets/images/shop_02.png',
+    'Quantity': 1
+},{
+  "id":"1",
+  "company": "Jordan",
+  "title": "Nike Jordan Running Shoes ",
+  'Size': [5,6,7,8],
+  'price': 60.5,
+'Deal': "59% OFF",
+    'Quantity': 1,
+
+  'imageURL': 'assets/images/shop_03.png',
+},{
+  "id":"2",
+  "company": "Puma",
+  "title": "Puma Viz Runner Running Shoes",
+  'Size': [5,6,7,8,9,10],
+  'price': 50.5,
+'Deal': "39% OFF",'Quantity': 1,
+
+  'imageURL': 'assets/images/shop_07.png',
+},{
+  "id":"3",
+  "company": "Adidas",
+  "title": "Adidas AdiDash Shoes for Men",
+  'Size': [5,6,7,8,9,10],
+  'price': 35.5,
+'Deal': "79% OFF",
+  'imageURL': 'assets/images/shop_02.png','Quantity': 1
+},{
+"id":"2",
+"company": "Puma",
+"title": "Puma Viz Runner Running Shoes",
+'Size': [5,6,7,8,9,10],
+'price': 50.5,
+'Deal': "39% OFF",
+'imageURL': 'assets/images/download.png','Quantity': 1
+},
+];
